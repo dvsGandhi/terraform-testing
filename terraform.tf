e9a2cafd5d3f18dc015dde2d8d7424c8fc8e43b9
dvsGandhi <107239389+dvsGandhi@users.noreply.github.com>
2024-08-21 17:39:50 +0530

    Add files via upload
    initial commit

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+# Configure Terraform to set the required AzureRM provider
+# version and features{} block.
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.74.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
